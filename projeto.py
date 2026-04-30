098949dbf214cd1f953b6b2d621ab783b6f2d221
mariaclaramendoncalopes-afk <mariaclaramendoncalopes@gmail.com>
2026-04-30 09:58:18 -0300

    menu Adm1

--- projeto.py
+++ projeto.py
@@ -33,11 +33,34 @@ while True:
                 usuario_encontrado = True
                 print(f'bem-vindo(a), {pessoas[0]}')
             else:
-                if cadastros == []:
-                    print('Não há nenhum usuário cadastrado.')
-                elif usuario_encontrado == False:
-                    print('usuário não encontrado.')
+               print('usuário não encontrado.')
             if pessoas[2] == 'adm': #continua com pessoas porque ainda está dentro do for
-                print('adm')
-            else:
+
+                print('---Menu (ADM)---')
+                rebanho = []
+                animais = ('bovino de leite', 'caprino', 'ovino', 'suíno', 'leitão')
+                print(f'Animais da fazenda: {animais}')
+                tipo = input('Informe qual o tipo do animal: ').lower()
+                if tipo not in animais:
+                    while tipo not in animais:
+                        print('Este animal não existe na fazenda sertão')
+                        tipo = input('Informe qual o tipo do animal: ')
+                
+                identificação = input('Existe algum tipo de identificação desse animal? (S/N): ').upper()
+                if identificação == 'S':
+                    identificação = input('Digite como identificar o animal: ')
+                else:
+                    identificação = 'Não identificavel'
+                
+                stats = ('lactação','engorda', 'disponível para venda', 'vendido')
+                print(f'Status dos animais: {stats}')
+                status = input('Informe o status do animal: ').lower()
+                if status not in stats:
+                    while status not in stats:
+                        print('Status não definido')
+                        status = input('Informe o status do animal: ')
+                rebanho.append([tipo, identificação, status])
+                print(rebanho)
+
+            elif pessoas[2] == 'cliente':
                 print('cliente')
