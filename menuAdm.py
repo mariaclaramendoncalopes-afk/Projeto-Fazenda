44cd3ce73cf555e34fc5ac83c076436bbfcb15ca
mariaclaramendoncalopes-afk <mariaclaramendoncalopes@gmail.com>
2026-05-01 10:46:23 -0300

    menu Adm2

--- menuAdm.py
+++ menuAdm.py
@@ -1,25 +1,155 @@
-print('---Menu (ADM)---')
-rebanho = []
-animais = ('bovino de leite', 'caprino', 'ovino', 'suíno', 'leitão')
-print(f'Animais da fazenda: {animais}')
-tipo = input('Informe qual o tipo do animal: ').lower()
-if tipo not in animais:
-    while tipo not in animais:
-        print('Este animal não existe na fazenda sertão')
-        tipo = input('Informe qual o tipo do animal: ')
+rebanho = [] # rebanho fora do while aaaaaaaaaaaaaaaaaa
+# no menu adm tem 2 variaveis 'op', a primeira é para o menu principal e a 'op2' é para o menu de alteração --> mudar os números qnd passar para o projeto
+while True:
+    print('---Menu (ADM)---')
+    print('1 - Cadastrar animal no rebanho')
+    print('2 - Buscar animal')
+    print('3 - Atualizar lista de animais')
+    op = int(input('Digite o que deseja fazer: '))
 
-identificação = input('Existe algum tipo de identificação desse animal? (S/N): ').upper()
-if identificação == 'S':
-    identificação = input('Digite como identificar o animal: ')
-else:
-    identificação = 'Não identificavel'
+    if op == 1: 
+        animaisF = ('bovino de leite', 'caprino', 'ovino', 'suino', 'leitao')
+        print(f'Animais da fazenda: {animaisF}')
+        tipo = input('Informe qual o tipo do animal: ').lower()
+        while tipo not in animaisF:
+            print('Este tipo de animal não existe na fazenda sertão')
+            print(f'Animais da fazenda: {animaisF}')
+            tipo = input('Informe qual o tipo do animal: ').lower()
 
-stats = ('lactação','engorda', 'disponível para venda', 'vendido')
-print(stats)
-status = input('Informe o status do animal: ').lower()
-if status not in stats:
-    while status not in stats:
-        print('Status não definido')
-        status = input('Informe o status do animal: ')
-rebanho.append([tipo, identificação, status])
-print(rebanho)
+        identificar = ('brinco', 'numero')
+        print(f'Tipos de identificação: {identificar}')
+        identificacao = input('Digite como identificar o animal: ').lower()
+        while identificacao not in identificar:
+            print('Digite uma identificação.')
+            print(f'Tipos de identificação: {identificar}')
+            identificacao = input('Digite como identificar o animal: ').lower()
+
+        numero_ID = int(input('Digite o número de identificação do animal: '))
+
+        stats = ('lactação','engorda', 'disponivel para venda', 'vendido')
+        print(f'status programados: {stats}')
+        status = input('Informe o status do animal: ').lower()
+        while status not in stats:
+            print('Status não definido')
+            print(f'status programados: {stats}')
+            status = input('Informe o status do animal: ').lower()
+
+        rebanho.append([tipo, identificacao, numero_ID, status])
+        print(rebanho)
+
+    elif op == 2:
+        busca = int(input('Informe o número do animal que está procurando: '))
+        achei = False
+        for animais in rebanho: # para animais(sublista de rebanho) em rebanho(lista)
+            if busca == animais[2]: #buscar na sublista (sublista - animais | lista - rebanho)
+                achei = True
+                print('Animal encontrado:', animais)
+                break
+            elif not achei:
+                print('Animal não encontrado')
+
+# Testar melhor
+    elif op ==3:
+        busca = int(input('Informe o número do animal que está procurando: '))
+        achei = False
+        for animais in rebanho:
+            if busca == animais[2]:
+                achei = True
+                print('Animal encontrado:', animais)
+
+                print()
+                print('1 - tipo')
+                print('2 - identificação')
+                print('3 - número de identificação')
+                print('4 - status do animal')
+                print('5 - alterar todo o cadastro')
+                op2 = int(input('Digite a opção que deseja fazer: '))
+                if op2 == 1:
+
+                    # animais = ('bovino de leite', 'caprino', 'ovino', 'suino', 'leitao')
+                    print(f'Animais da fazenda: {animaisF}')
+                    novo_tipo = input('Informe qual o tipo do animal: ').lower()
+                    while novo_tipo not in animaisF:
+                        print('Este tipo de animal não existe na fazenda sertão')
+                        print(f'Animais da fazenda: {animaisF}')
+                        novo_tipo = input('Informe qual o tipo do animal: ').lower()
+                        #alteração na lista:
+                    animais[0] = novo_tipo
+                    print(f'Tipo de animal alterado: novo tipo = {novo_tipo}')
+                    break
+
+                elif op2 == 2:
+                    print(f'Tipos de identificação: {identificar}')
+                    nova_identificacao = input('Digite como identificar o animal: ').lower()
+                    while nova_identificacao not in identificar:
+                        print('Digite uma identificação.')
+                        print(f'Tipos de identificação: {identificar}')
+                        nova_identificacao = input('Digite como identificar o animal: ').lower()
+                    animais[1] = nova_identificacao
+                    print(f'Tipo de identificação alterada: nova identificação = {nova_identificacao}')
+
+                    novo_ID = input('O número de identificação será alterado? (S/N)').upper()
+                    if novo_ID == 'S':
+                        novo_ID = int(input('Digite o novo número de identificação: '))
+                        while novo_ID == numero_ID:
+                            print('ID iguais, digite novamente: ')
+                            novo_ID = int(input('Digite o novo número de identificação'))
+                        animais[2] = novo_ID
+                        print(f'Número de identificação alterado: novo número = {novo_ID}')
+                    else:
+                        break
+
+                elif op2 == 3:
+                    novo_ID = int(input('Digite o novo número de identificação: '))
+                    while novo_ID == numero_ID:
+                        print('ID iguais, digite novamente: ')
+                        novo_ID = int(input('Digite o novo número de identificação: '))
+                    animais[2] = novo_ID
+                    print(f'Número de identificação alterado: novo número = {novo_ID}')
+                    break
+
+                elif op2 == 4:
+                    print(f'status programados: {stats}')
+                    novo_status = input('Informe o status do animal: ').lower()
+                    while novo_status not in stats:
+                        print('Status não definido')
+                        print(f'status programados: {stats}')
+                        novo_status = input('Informe o status do animal: ').lower()
+                    animais[3] = novo_status
+                    print(f'Status alterado: novo status = {novo_status}')
+                    break
+
+                elif op2 == 5:
+                    print(f'Animais da fazenda: {animaisF}')
+                    novo_tipo = input('Informe qual o tipo do animal: ').lower()
+                    while novo_tipo not in animaisF:
+                        print('Este tipo de animal não existe na fazenda sertão')
+                        print(f'Animais da fazenda: {animaisF}')
+                        novo_tipo = input('Informe qual o tipo do animal: ').lower()
+                    animais[0] = novo_tipo
+
+                    print(f'Tipos de identificação: {identificar}')
+                    nova_identificacao = input('Digite como identificar o animal: ').lower()
+                    while nova_identificacao not in identificar:
+                        print('Digite uma identificação válida.')
+                        print(f'Tipos de identificação: {identificar}')
+                        nova_identificacao = input('Digite como identificar o animal: ').lower()
+                    animais[1] = nova_identificacao
+                    
+                    novo_ID = int(input('Digite o novo número de identificação: '))
+                    while novo_ID == numero_ID:
+                        print('ID iguais, digite novamente: ')
+                        novo_ID = int(input('Digite o novo número de identificação: '))
+                    animais[2] = novo_ID
+
+                    print(f'status programados: {stats}')
+                    novo_status = input('Informe o novo status do animal: ').lower()
+                    while novo_status not in stats:
+                        print('Status não definido')
+                        print(f'status programados: {stats}')
+                        novo_status = input('Informe o novo status do animal: ').lower()
+                    animais[3] = novo_status
+                    print(animais)
+                    break
+        if not achei:
+            print('Animal não encontrado')
--- projeto.py
+++ projeto.py
@@ -1,16 +1,63 @@
 cadastros = []
+adms = [['kamille', '1234'], ['clara', '5678']]
 while True:
-    print('--- Fazenda Sertão ---')
-    print('1 - criar perfil')
-    print('2 - Entrar')
-    op = int(input('Qual opção deseja fazer? '))
-
+    print('='*10,   'MENU  PRINCIPAL  |  FAZENDA SERTÃO',   '='*10 )
+    print('1 - CADRASTRAR USUÁRIO')
+    print('2 - LOGIN')
+    print('3 - SAIR')
+    print('\n')
+    op = int(input('QUAL OPÇÃO VOCÊ DESEJA REALIZAR:   '))
 
     if op == 1:
 
-        usuario = input('Digite seu nome de usuário: ')
-        senha = input('Digite sua senha: ')
-        tipo = input('Digite o tipo de usuário (ADM / Cliente): ').lower()
+        usuario = input('Crie um nome de usuário:  ').lower()
+
+        while True:
+            senha = input('Crie sua senha: ')
+            
+            if len(senha) <= 3:
+                print('Senha curta | Deve conter +3 caracteres.\n')
+                continue
+            elif senha == usuario:
+                print('Senha fraca! Não pode conter o mesmo nome do usuário.\n')
+                continue
+            
+            confirmacao = input('Confirme sua senha:')
+            print('\n')
+            
+            if senha != confirmacao:
+                print('As senhas não coincidem. Tente novamente.\n')
+            else:
+                cadastros.append([usuario, senha])
+                break
+
+        print('Usuário cadastrado com sucesso! | Faça o login')
+        print()
+        
+
+
+    if op == 2:
+        usuario = input('Usuário:  ').lower ()
+        senha = input('Senha:  ')
+
+        for adm in adms:
+            if usuario == adm[0] and senha == adm[1]:
+                print('Login ADMIN')
+                break
+        else:
+            for pessoa in cadastros:
+                if usuario == pessoa[0] and senha == pessoa[1]:
+                    print('Login CLIENTE')
+                    break
+            else:
+                print('Usuário ou senha incorretos')
+        break            
+
+
+
+
+#FAZENDO ALTERAÇÕES
+'''
 
         if tipo != 'adm' and tipo != 'cliente':
             while tipo != 'adm' and tipo != 'cliente':
@@ -32,35 +79,51 @@ while True:
             if usuario_entrar == pessoas[0] and senha_entrar == pessoas[1]:
                 usuario_encontrado = True
                 print(f'bem-vindo(a), {pessoas[0]}')
-            else:
-               print('usuário não encontrado.')
-            if pessoas[2] == 'adm': #continua com pessoas porque ainda está dentro do for
+            # retirar o else, porque msm achando o usuário ele ainda imprime q não encontrou
+            #  utilizar só dps dos menus, se não ainda vai aparecer
 
-                print('---Menu (ADM)---')
-                rebanho = []
-                animais = ('bovino de leite', 'caprino', 'ovino', 'suíno', 'leitão')
-                print(f'Animais da fazenda: {animais}')
-                tipo = input('Informe qual o tipo do animal: ').lower()
-                if tipo not in animais:
-                    while tipo not in animais:
-                        print('Este animal não existe na fazenda sertão')
-                        tipo = input('Informe qual o tipo do animal: ')
-                
-                identificação = input('Existe algum tipo de identificação desse animal? (S/N): ').upper()
-                if identificação == 'S':
-                    identificação = input('Digite como identificar o animal: ')
-                else:
-                    identificação = 'Não identificavel'
-                
-                stats = ('lactação','engorda', 'disponível para venda', 'vendido')
-                print(f'Status dos animais: {stats}')
-                status = input('Informe o status do animal: ').lower()
-                if status not in stats:
-                    while status not in stats:
-                        print('Status não definido')
-                        status = input('Informe o status do animal: ')
-                rebanho.append([tipo, identificação, status])
-                print(rebanho)
+                if pessoas[2] == 'adm': #continua com 'pessoas' porque ainda está dentro do for
+                    # o if dos menus deve estar dentro do if de login para evitar o loop
+                    rebanho = []
+                    while True:
+                        print('---Menu (ADM)---')
+                        print('1 - Cadastrar animal no rebanho')
+                        op = int(input('Digite o que deseja acessar: '))
 
-            elif pessoas[2] == 'cliente':
-                print('cliente')
+                        if op == 1:
+                            animais = ('bovino de leite', 'caprino', 'ovino', 'suino', 'leitao')
+                            print(f'Animais da fazenda: {animais}')
+                            tipo = input('Informe qual o tipo do animal: ').lower()
+
+                            while tipo not in animais:
+                                print('Este tipo de animal não existe na fazenda sertão')
+                                print(f'Animais da fazenda: {animais}')
+                                tipo = input('Informe qual o tipo do animal: ').lower()
+                            
+                            identificar = ('brinco', 'numero')
+                            print(f'Tipos de identificação: {identificar}')
+                            identificacao = input('Digite como identificar o animal: ').lower()
+                            while identificacao not in identificar:
+                                print('Digite uma identificação.')
+                                print(f'Tipos de identificação: {identificar}')
+                                input('Digite como identificar o animal: ').lower()
+
+                            numero_ID = int(input('Digite o número de identificação do animal: '))
+                            
+                            stats = ('lactação','engorda', 'disponivel para venda', 'vendido')
+                            print(f'Status dos animais: {stats}')
+                            status = input('Informe o status do animal: ').lower()
+
+                            while status not in stats:
+                                print('Status não definido')
+                                print(f'Status dos animais: {stats}')
+                                status = input('Informe o status do animal: ').lower()
+
+                            rebanho.append([tipo, identificacao, numero_ID, status])
+                            print(rebanho)
+                            break
+
+                elif pessoas[2] == 'cliente':
+                    print('cliente')
+                    break
+'''
