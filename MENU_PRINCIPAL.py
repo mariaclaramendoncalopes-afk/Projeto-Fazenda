d8753c2dc36615c652d0532d1b56be14399d367b
kamillevieiira <kamillevieira44@gmail.com>
2026-05-02 15:43:55 -0300

    menu_adm

---
+++ MENU_PRINCIPAL.py
@@ -0,0 +1,54 @@
+usuarios = [
+    # nome, senha, eh_admin
+    ['kamille', '1234', True],
+    ['clara', '5678', True],
+]
+
+while True:
+    print('='*10,   'MENU  PRINCIPAL  |  FAZENDA SERTÃO',   '='*10)
+    print('1 - CADRASTRAR USUÁRIO')
+    print('2 - LOGIN')
+    print('3 - SAIR')
+    print('\n')
+    op = input('QUAL OPÇÃO VOCÊ DESEJA REALIZAR:   ')
+
+    if op == '1':
+        usuario = input('Crie um nome de usuário:  ')
+
+        while True:
+            senha = input('Crie sua senha: ')
+
+            if len(senha) <= 3:
+                print('Senha curta | Deve conter +3 caracteres.\n')
+                continue
+
+            if senha == usuario:
+                print('Senha fraca! Não pode conter o mesmo nome do usuário.\n')
+                continue
+
+            confirmacao = input('Confirme sua senha:')
+            print('\n')
+
+            if senha != confirmacao:
+                print('As senhas não coincidem. Tente novamente.\n')
+                continue
+
+            usuarios.append([usuario, senha, False])
+            break
+
+        print('Usuário cadastrado com sucesso! | Faça o login')
+        print()
+
+    if op == '2':
+        usuario = input('Usuário:  ').lower()
+        senha = input('Senha:  ')
+
+        for pessoa in usuarios:
+            if usuario == pessoa[0] and senha == pessoa[1] and pessoa[2] == True:
+                print('Login ADMIN')
+                break
+            if usuario == pessoa[0] and senha == pessoa[1] and pessoa[2] == False:
+                print('Login CLIENTE')
+                break
+        else:
+            print('Usuário não encontrado')
